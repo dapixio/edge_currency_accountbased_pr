2eecebb2ccd383e1cc32478b6a14d2fde5dee1ae
davidfaerman <david.faerman@bairesdev.com>
2019-07-15 15:55:26 -0300

    FIO PR

--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@ import { makeEosPlugin } from './eos/eosPlugin.js'
 import { makeEthereumPlugin } from './ethereum/ethPlugin.js'
 import { makeStellarPlugin } from './stellar/stellarPlugin.js'
 import { makeRipplePlugin } from './xrp/xrpPlugin.js'
+import { makeFioPlugin } from './fio/fioPlugin'
 
 const plugins = {
   eos: makeEosPlugin,
   ethereum: makeEthereumPlugin,
   // "ripple" is network name. XRP is just an asset:
   ripple: makeRipplePlugin,
-  stellar: makeStellarPlugin
+  stellar: makeStellarPlugin,
+  fio: makeFioPlugin
 }
 
 if (
